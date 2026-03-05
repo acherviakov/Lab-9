9d6f95b27de0cea77c28e4c92e90b0964c22b84a
acherviakov <artem21072010@mail.ru>
2026-03-05 08:22:44 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -126,25 +126,30 @@ def is_number(string):
     return True
 '''
 Предложить два вида работы: демонстрационный (число 1) и интерактивный (число 2).
-Если пользователь вводит число 1, то запустить демонстрационный режим.
-Если пользователь вводит число 2, то запустить интерактивный режим.
-Если пользователь вводит что-то другое, сказать, что нужно ввести 1 или 2, повторить шаг 1.
+Если пользователь вводит число 1, 
+то запустить демонстрационный режим.
+Если пользователь вводит число 2, 
+то запустить интерактивный режим.
+Если пользователь вводит что-то другое, сказать, 
+что нужно ввести 1 или 2, 
+повторить шаг 1.
 '''
 def start():
-    mode = input()
     print("Выберите режим работы: интерактивный (введите 2) или демонстративный (введите 1)")
+    mode = input()
     if mode == "1":
         run_demo_mode()
     elif mode == "2":
         run_interactive_mode()
     else:
-        print(False)
+        start()
 
 '''
 Если режим работы демонстрационный, то:
 1. создадим массив из 1000 случайных элементов от 0 до 99.
-2. отсортировать массив пузырьком, простым обменом
+2. отсортировать массив пузырьком (простым обменом)
 3. отсортировать массив вставками
+4. отсортировать способом
 5. вывести режим результатов
 6. предложить выйти, введя любую строку
 '''
@@ -175,12 +180,21 @@ def run_demo_mode():
 6. Спросить пользователя о его дальнейших действиях.
 7. Если пользователь ввёл 1, то замена части массива:
     Вводится начальный индекс замены, часть массива.
-    +проверка, чтобы введенные данные являлись числами и начальный индекс находится от 0 до длины массива, иначе повторить ввод
-    +проверить, что сумма индекса замены и длины части массива не больше, чем индекс последнего элемента, иначе повторить ввод
-8. Если пользователь ввёл 2, то вывести массив (отсортированный)
-8+. Если пользователь ввёл 3, то вывести неотсортированный массив
-9. Если пользователь ввёл 4, то повторить сначала
-10. Если пользователь ввёл 5, то выйти из режима
+    +проверка, чтобы введенные данные являлись числами 
+    и начальный индекс находится от 0 до длины массива, 
+    иначе повторить ввод
+    +проверить, что сумма индекса замены 
+    и длины части массива не больше, чем индекс последнего элемента, 
+    иначе повторить ввод
+    +следующие n элемнтов, где n - длина части массива, заменим элементы из этого массива
+8. Если пользователь ввёл 2,
+ то вывести массив (отсортированный)
+8+. Если пользователь ввёл 3, 
+то вывести неотсортированный массив
+9. Если пользователь ввёл 4, 
+то повторить сначала
+10. Если пользователь ввёл 5, 
+то выйти из режима
 11. Во всех остальных случаях повторить ввод
 
 
@@ -197,14 +211,15 @@ def run_demo_mode():
 def show_table(array, unsorted_array):
     table = []
     table.append(["Сортировка пузырьком"] + bubble_sort(array)[1:])
+    array = unsorted_array.cpoy()
     table.append(["Cортировка простым выбором"] + selection_sort(array)[1:])
-    array = unsorted_array
+    array = unsorted_array.copy()
     table.append(["Cортировка вставками"] + insertion_sort(array)[1:])
     headers = ["Сортировка", "Сравнения", "Перестановки" ]
     print(tabulate(table, headers=headers))
 
 '''
-Проверим, что все элемнты списка -  числа
+Проверим, что все элемнты списка - числа
 '''
 def is_number_array(array):
     for element in array:
@@ -215,32 +230,48 @@ def is_number_array(array):
 def action(array, unsorted_array):
     action_code = input()
     if action_code == "1":
+        starter_position = 0
         print("Введите индекс, начиная с которого хотите сделать замену")
-        possible = True
+        possible = False
         while not possible:
-            starter = input()
-            if(not is_number(starter)):
-                print("Введите число, а не " + starter)
+            value = input()
+            if(not is_number(value)):
+                print("Введите число, а не " + value)
             else:
-                if int(starter) >= len(unsorted_array):
+                if int(value) >= len(unsorted_array):
                     print("Ошибка. Стартовый элемент не должен лежать за пределами массива")
                 else:
                     possible = True
-            starter_position = int(starter)
+                    starter_position = int(value)
+        print("Введите массив (числа через пробел), на который вы хотите заменить часть исх. массива")
+        possible = False
+        number_replacement_list = []
+        while not possible:
+            value = input().split()
+            if(not is_number_array(value)):
+                print("Не все элементы массива - числа. Введите числа, разделенные пробелом")
+            else:
+                if len(value) + starter_position > len(array):
+                    print("Ошибка. Конечный элемент за рамками массива.")
+                else:
+                    possible = True
+                    number_replacement_list = list(map(int, value))
+        unsorted_array = unsorted_array[0:starter_position]+number_replacement_list+unsorted_array[starter_position+len(number_replacement_list):]
+        action(array, unsorted_array)
         
 
 
 
     elif action_code == "2":
         print(array)
-        action()
+        action(array, unsorted_array)
     elif action_code == "3":
         print(unsorted_array)
-        action()
+        action(array, unsorted_array)
     elif action_code == "4":
-        array = unsorted_array
+        array = unsorted_array.copy()
         show_table(array, unsorted_array)
-        action()
+        action(array, unsorted_array)
     else:
         start()
 
@@ -259,13 +290,8 @@ def run_interactive_mode():
                 size_possible = True
     array = [randint(0, 99) for index in range(int(size))]
     unsorted_array = array.copy()
-    table = []
-    table.append(["Сортировка пузырьком"] + bubble_sort(array)[1:])
-    table.append(["Cортировка простым выбором"] + selection_sort(array)[1:])
-    array = unsorted_array
-    table.append(["Cортировка вставками"] + insertion_sort(array)[1:])
-    headers = ["Сортировка", "Сравнения", "Перестановки" ]
-    print(tabulate(table, headers=headers))
+    show_table(array, unsorted_array)
     print("Чтобы изменить массив (изначальный), введите 1. Чтобы вывести его (будет выведен отсортированный), введите 2. Чтобы вывести неотсортированный (изначальный) массив, введите 3. Чтобы отсорировать ещё раз, введите 4. Чтобы выйти, введите любую другую строку")
+    action(array, unsorted_array)
     
 start()
